8adf758713c18bdb0dc0f16cab9e2da78ed3d218
Dillon Stadther <dlstadther@gmail.com>
2019-09-18 19:34:46 -0400

    Update tests/pytest/func.py to pass pylint

--- tests/pytest/test_func.py
+++ tests/pytest/test_func.py
@@ -1,3 +1,6 @@
+"""
+Module docstring goes here
+"""
 import datetime
 
 import pytest
@@ -10,6 +13,11 @@ from py_testing.func import bom, eom
     pytest.param(datetime.date(2019, 2, 15), datetime.date(2019, 2, 1), id='2019-02'),
 ])
 def test_bom(in_date, expected_date):
+    """
+    Test BOM func
+    :param in_date: input to func
+    :param expected_date: expected output to func
+    """
     assert bom(in_date) == expected_date
 
 
@@ -19,8 +27,16 @@ def test_bom(in_date, expected_date):
     pytest.param(datetime.date(2020, 2, 15), datetime.date(2020, 2, 29), id='2020-02'),
 ])
 def test_eom(in_date, expected_date):
+    """
+    Test EOM func
+    :param in_date: input to func
+    :param expected_date: expected output to func
+    """
     assert eom(in_date) == expected_date
 
 
 def test_fail():
+    """
+    Test expected to fail
+    """
     assert 0 == 1
